dbbdddf2a031066b51c643324b9ddeccf2fbd4b4
dlabaja <dlabaja.jan@seznam.cz>
2021-11-16 15:44:03 +0100

    formulář

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request
 import random
+import config
 
 app = Flask(__name__)
+app.secret_key = config.secret
 
 #git test
-#jinja-html v include, hodnota html
+#jinja-html v include, hodnota html, inc-lan
 
 @app.route("/")
 def index():
     return render_template("index.html.j2")
+
+@app.route("/t")
+def test():
+    name = request.args.get("name")
+    password = request.args.get("password")
+    if name:
+        session["cookie"] = name,password
+    print(session)
+    return render_template("test.html.j2"), session
 
 @app.errorhandler(404)
 def page_not_found(e):
