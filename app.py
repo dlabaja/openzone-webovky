91e37e234208747d9a3ff5595e6958c6e95812a3
dlabaja <dlabaja.jan@seznam.cz>
2021-11-24 18:59:50 +0100

    form frontend

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, session, request
-import random
+from pymongo import MongoClient
+import pymongo
 import config
 
 app = Flask(__name__)
 app.secret_key = config.secret
+client = MongoClient(config.connection_string)
 
 #git test
 #jinja-html v include, hodnota html, inc-lan
@@ -12,14 +14,19 @@ app.secret_key = config.secret
 def index():
     return render_template("index.html.j2")
 
-@app.route("/t")
-def test():
+@app.route("/form")
+def form():
     name = request.args.get("name")
     password = request.args.get("password")
     if name:
         session["cookie"] = name,password
     print(session)
-    return render_template("test.html.j2"), session
+    return render_template("form.html.j2"), session
+
+@app.route("/voted")
+def form_completed():
+    return render_template("form.html.j2")
+
 
 @app.errorhandler(404)
 def page_not_found(e):
