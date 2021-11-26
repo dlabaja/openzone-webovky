274db442ba0141d4e4bbe61e9773b000aaa6622a
dlabaja <dlabaja.jan@seznam.cz>
2021-11-26 19:07:18 +0100

    bez db, funkční form

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, session, request
 from pymongo import MongoClient
+from flask_wtf import FlaskForm
+from wtforms import StringField, RadioField, widgets 
+from wtforms.validators import InputRequired
 import pymongo
 import config
 
@@ -7,30 +10,33 @@ app = Flask(__name__)
 app.secret_key = config.secret
 client = MongoClient(config.connection_string)
 
-#git test
-#jinja-html v include, hodnota html, inc-lan
-
 @app.route("/")
 def index():
     return render_template("index.html.j2")
 
-@app.route("/form")
+@app.route("/form", methods=['GET', 'POST'])
 def form():
-    name = request.args.get("name")
-    password = request.args.get("password")
-    if name:
-        session["cookie"] = name,password
-    print(session)
-    return render_template("form.html.j2"), session
-
-@app.route("/voted")
-def form_completed():
-    return render_template("form.html.j2")
-
-
+    form = Form()    
+    if not hasVoted():
+        if form.validate_on_submit():
+            name = form.name.data
+            choice = form.choice.data
+            print(name + choice)
+            #session["voted"] = True
+            return render_template("form_completed.html.j2", form = form)
+        return render_template("form.html.j2", form = form)
+    return render_template("form_completed.html.j2", form = form)
+    
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html.j2')
 
+class Form(FlaskForm):
+    name = StringField("Jméno", widget = widgets.Input(input_type = "text"), validators=[InputRequired("Musíte zadat jméno")])
+    choice = RadioField("Vyberte možnost", choices=[("js" ,"Javascript"), ("clang", "C lang"), ("py", "Python")], validators=[InputRequired("Musíte zadat možnost")])
 
-
+def hasVoted():
+    for item in session:
+        if item == "voted":
+            return True
+    return False
