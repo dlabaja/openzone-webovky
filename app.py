bd938253751956b607a4d59c6267461d07744eaf
dlabaja <dlabaja.jan@seznam.cz>
2021-12-11 22:08:52 +0100

    hotový backend, admin funkce stránky, dělal jsem na tom celej den konečně můžu jít spát

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, session, request, redirect, url_for
+from flask import Flask, render_template, session, request, redirect, url_for, abort
 from pymongo import MongoClient
 from flask_wtf import FlaskForm
 from wtforms import StringField, RadioField, widgets , EmailField, PasswordField, validators
 from wtforms.validators import InputRequired, Length, EqualTo, Email, DataRequired
 from bson.objectid import ObjectId
 import email_validator
-import config, os, hashlib
+import config, os, hashlib, random
 
 app = Flask(__name__)
 app.secret_key = config.secret
@@ -16,13 +16,37 @@ _coll = db["form"]
 _usercoll = db["users"]
 _votedcoll = db["voted"]
 
-#registrace vygeneruje klíč, uložení do cookie, přihlášení podle klíče
-#login stránka přesměruje na domovskou. nahradí jméno v rohu
-
 @app.route("/")
 def index():
     votes = getVotes()
-    return render_template("index.html.j2", labels = votes[0], values = votes[1])
+    return render_template("index.html.j2", labels = votes[0], values = votes[1], colors = hexGenerator(len(votes[0])))
+
+@app.route("/edit", methods=['GET','POST'])
+def edit():
+    if(session.get("admin") == True):
+        form = AddToForm()
+        if form.validate_on_submit():
+            global _coll
+            coll = _coll
+            query = coll.find_one({"_id": ObjectId(config.form_id)})
+            newvalue = {"$set":{f"votes.{form.choice.data}":0}}
+            coll.update_one(dict(query), newvalue)
+
+        return render_template("edit.html.j2", names = getNameCollection(), votes = getVoteCollection(), form = form)
+    abort(404)
+
+@app.route("/dropdb")
+def dropdb():
+    if(session.get("admin") == True):
+        RemoveVotes()
+        return redirect("/edit")
+    abort(404)
+
+@app.route("/dropdb", methods=["POST"])
+def _dropdb():
+    if(session.get("admin") == True):
+        return redirect("/edit")
+    abort(404)
 
 @app.route("/form", methods=['GET', 'POST'])
 def form():
@@ -51,12 +75,14 @@ def login():
             return render_template('login.html.j2', form = form)
         if comparePasswords(data[2], data[3], form.password.data):
             session["user"] = data[0]
+            session["admin"] = data[4]
             return redirect(url_for("index")) 
     return render_template('login.html.j2', form = form)
 
 @app.route("/logout")
 def logout():
     session.pop("user")
+    session.pop("admin")
     return redirect(url_for("index")) 
 
 def getLoginInfo(username):
@@ -68,7 +94,7 @@ def getLoginInfo(username):
         return False
     query = dict(query)
     print("aaa")
-    return query.get("name"), query.get("email"), query.get("password"), query.get("salt")
+    return query.get("name"), query.get("email"), query.get("password"), query.get("salt"), query.get("admin")
 
 @app.route("/register", methods=['GET','POST'])
 def register_post():
@@ -90,7 +116,8 @@ def setRegister(name, email, password):
     bson = {"name": name,
             "email":email,
             "password": hashedpsw[1],
-            "salt": hashedpsw[0]}
+            "salt": hashedpsw[0],
+            "admin":False}
     query = coll.insert_one(bson)
 
 def hash(password):
@@ -105,7 +132,10 @@ def comparePasswords(dbpassword, salt, currentpassword):
     return False
 
 class Form(FlaskForm):
-    choice = RadioField("Vyberte možnost", choices=[("Javascript" ,"Javascript"), ("C lang", "C lang"), ("Python", "Python")], validators=[InputRequired("Musíte zadat možnost")])
+    choice = RadioField("Vyberte možnost", choices=config.getChoices(), validators=[InputRequired("Musíte zadat možnost")])
+
+class AddToForm(FlaskForm):
+    choice = StringField("Volba", widget = widgets.Input(input_type = "text"), validators=[InputRequired("Musíte zadat volbu")])
 
 class RegisterForm(FlaskForm):
     name = StringField("Jméno", widget = widgets.Input(input_type = "text"), validators=[InputRequired("Musíte zadat jméno")])
@@ -130,26 +160,66 @@ def vote(choice):
     coll = _coll
     global _votedcoll
     votedcoll = _votedcoll
+    print(choice)
+    query = coll.find_one({"_id": ObjectId(config.form_id)})
+    coll.update_one(dict(query), { "$set": { f"votes.{choice}":  int(dict(query).get("votes").get(choice)) + 1}})
 
-    query = coll.find_one({"_id": ObjectId("619d38f7b9673a94534feb35")})
-    newvalues = { "$set": { choice:  int(dict(query).get(choice)) + 1}, "$addToSet":{"names":f"{session.get('user')}, {choice}"} }
-    coll.update_one(dict(query), newvalues)
+    query_name = coll.find_one({"_id": ObjectId(config.names_id)})
+    coll.update_one(dict(query_name), {"$addToSet":{"names":f"{session.get('user')}, {choice}"}})
+
     votedcoll.insert_one({"user":session.get("user")})
     
 
 def getVotes():
     global _coll
     coll = _coll
-    query = dict(coll.find_one({"_id": ObjectId("619d38f7b9673a94534feb35")}))
+    query = dict(coll.find_one({"_id": ObjectId(config.form_id)}))
     labels = []
     values = []
 
-    for item in query:
-        if item != "_id" and item != "names":
+    for item in query.get("votes"):
             labels.append(item)          
-            values.append(query.get(item))
+            values.append(query.get("votes").get(item))
 
     return labels, values
+
+def getNameCollection():
+    global _coll
+    coll = _coll
+    query = dict(coll.find_one({"_id": ObjectId(config.names_id)}, {"_id": 0 }))
+    string = "<b>Již hlasovali:</b><br>"
+    for _item in query.values():
+        for item in _item:
+            string =string+ f"{item}<br>"
+    return string
+
+def getVoteCollection():
+    global _coll
+    coll = _coll
+    query = dict(coll.find_one({"_id": ObjectId(config.form_id)}, {"_id": 0 }))
+    string = "<b>Možnosti ankety:</b><br>"
+    for _item in query.values():
+        for item in _item:
+            string =string+ f"{item}<br>"
+    return string
+
+def hexGenerator(count):
+    colors = []
+    for x in range(count):
+        r = lambda: random.randint(0,255)
+        colors.append('#%02X%02X%02X' % (r(),r(),r()))
+    return colors
+
+def RemoveVotes():
+    global _coll
+    coll = _coll
+    query = coll.find_one({"_id": ObjectId(config.form_id)},{"votes":1,"_id":0})
+    print(query)
+  
+    bson = {"votes":{}}
+    coll.replace_one(query, bson)
+    
+
 
     
     
