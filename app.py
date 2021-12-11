ba30aebeb0383cb7263e0f06c6a84cdb33f8e614
dlabaja <dlabaja.jan@seznam.cz>
2021-12-11 12:11:11 +0100

    funkční login

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 from flask_wtf import FlaskForm
 from wtforms import StringField, RadioField, widgets , EmailField, PasswordField, validators
 from wtforms.validators import InputRequired, Length, EqualTo, Email, DataRequired
+from bson.objectid import ObjectId
 import email_validator
 import config, os, hashlib
 
@@ -13,6 +14,7 @@ client = MongoClient(config.connection_string)
 db = client["openzone"]
 _coll = db["form"]
 _usercoll = db["users"]
+_votedcoll = db["voted"]
 
 #registrace vygeneruje klíč, uložení do cookie, přihlášení podle klíče
 #login stránka přesměruje na domovskou. nahradí jméno v rohu
@@ -25,16 +27,16 @@ def index():
 @app.route("/form", methods=['GET', 'POST'])
 def form():
     form = Form()    
-    if not hasVoted:
-        if form.validate_on_submit():
-            name = form.name.data
-            choice = form.choice.data
-            vote(choice, name)
+    if session.get("user", False):
+        if not hasVoted():
+            if form.validate_on_submit():
+                choice = form.choice.data
+                vote(choice)
 
-            session["voted"] = True
-            return render_template("form_completed.html.j2", form = form)
-        return render_template("form.html.j2", form = form)
-    return render_template("form_completed.html.j2", form = form)
+                return render_template("form_completed.html.j2", form = form)
+            return render_template("form.html.j2", form = form)
+        return render_template("form_completed.html.j2", form = form)
+    return render_template("acces_denied.html.j2", form = form)
     
 @app.errorhandler(404)
 def page_not_found(e):   
@@ -42,18 +44,31 @@ def page_not_found(e):
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    form = LoginForm() 
-    data = getLoginInfo()
-    if session.get("user", False):
-        
+    form = LoginForm()
+    if form.validate_on_submit(): 
+        data = getLoginInfo(form.name.data)
+        if not data:
+            return render_template('login.html.j2', form = form)
+        if comparePasswords(data[2], data[3], form.password.data):
+            session["user"] = data[0]
+            return redirect(url_for("index")) 
     return render_template('login.html.j2', form = form)
+
+@app.route("/logout")
+def logout():
+    session.pop("user")
+    return redirect(url_for("index")) 
 
 def getLoginInfo(username):
     global _coll
     coll = _usercoll
-    query = dict(coll.find_one({"name": username}))
-
-    return query.get("name"), query.get("email"), query.get("password")
+    query = coll.find_one({"name": username})   
+    
+    if query == None:
+        return False
+    query = dict(query)
+    print("aaa")
+    return query.get("name"), query.get("email"), query.get("password"), query.get("salt")
 
 @app.route("/register", methods=['GET','POST'])
 def register_post():
@@ -71,22 +86,25 @@ def register_post():
 def setRegister(name, email, password):
     global _coll
     coll = _usercoll
+    hashedpsw = hash(password)
     bson = {"name": name,
             "email":email,
-            "password": hash(password)}
+            "password": hashedpsw[1],
+            "salt": hashedpsw[0]}
     query = coll.insert_one(bson)
 
 def hash(password):
     salt = os.urandom(32)
     key = hashlib.pbkdf2_hmac("sha256", str(password).encode("utf-8"), salt, 100000)
-    print(salt)
-    print(key)
-    storage = salt + key
-    return storage
+    return salt, key
 
+def comparePasswords(dbpassword, salt, currentpassword):
+    psw = hashlib.pbkdf2_hmac("sha256", str(currentpassword).encode("utf-8"), salt, 100000)
+    if dbpassword == psw:
+        return True
+    return False
 
 class Form(FlaskForm):
-    name = StringField("Jméno", widget = widgets.Input(input_type = "text"), validators=[InputRequired("Musíte zadat jméno")])
     choice = RadioField("Vyberte možnost", choices=[("Javascript" ,"Javascript"), ("C lang", "C lang"), ("Python", "Python")], validators=[InputRequired("Musíte zadat možnost")])
 
 class RegisterForm(FlaskForm):
@@ -100,19 +118,24 @@ class LoginForm(FlaskForm):
     password = PasswordField("Heslo", validators=[InputRequired("Musíte zadat heslo")])
 
 def hasVoted():
-    for item in session:
-        if item == "voted":
-            return True
-    return False
+    global _votedcoll
+    votedcoll = _votedcoll
+    query = votedcoll.find_one({"user": session.get("user")})
+    if query == None:
+        return False
+    return True
 
-def vote(choice, name):
+def vote(choice):
     global _coll
     coll = _coll
+    global _votedcoll
+    votedcoll = _votedcoll
 
-    from bson.objectid import ObjectId
     query = coll.find_one({"_id": ObjectId("619d38f7b9673a94534feb35")})
-    newvalues = { "$set": { choice:  int(dict(query).get(choice)) + 1}, "$addToSet":{"names":f"{name}, {choice}"} }
+    newvalues = { "$set": { choice:  int(dict(query).get(choice)) + 1}, "$addToSet":{"names":f"{session.get('user')}, {choice}"} }
     coll.update_one(dict(query), newvalues)
+    votedcoll.insert_one({"user":session.get("user")})
+    
 
 def getVotes():
     global _coll
@@ -121,14 +144,10 @@ def getVotes():
     labels = []
     values = []
 
-    from bson.objectid import ObjectId
     for item in query:
         if item != "_id" and item != "names":
-            labels.append(item)
-    
-    for item in query:
-        if item != ObjectId("619d38f7b9673a94534feb35") and type(item) != list:
-            values.append(item)
+            labels.append(item)          
+            values.append(query.get(item))
 
     return labels, values
 
